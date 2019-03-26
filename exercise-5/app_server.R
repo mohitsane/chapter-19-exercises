52032cc18f247c7d84d58f5a56563560312a1869
mkfreeman <michael.k.freeman@gmail.com>
2019-03-26 10:07:04 -0700

    ex-5 update to using app.R structure, break ui into multiple components

--- exercise-5/app_server.R
+++ exercise-5/app_server.R
@@ -6,21 +6,18 @@ library(ggplot2)
 
 # It should use an `input` with features: `x_var`, `y_var`, `color`, and `size`
 # Save the result of `renderPlot` to output$scatter
-shinyServer(function(input, output) {
+server <- function(input, output) {
   output$scatter <- renderPlot({
-    # Store x and y values to plot
-    x <- mpg[[input$x_var]]
-    y <- mpg[[input$y_var]]
     
-    # Store the title of the graph in a variable
+    # Store the title of the graph in a variable indicating the x/y variables
     title <- paste0("MPG Dataset: ", input$x_var, " v.s.", input$y_var)
     
     # Create ggplot scatter
-    p <- ggplot() +
-      geom_point(mapping = aes(x = x, y = y), 
+    p <- ggplot(mpg) +
+      geom_point(mapping = aes_string(x = input$x_var, y = input$y_var), 
         size = input$size, 
         color = input$color) +
       labs(x = input$x_var, y = input$y_var, title = title)
     p
   })
-})
+}
